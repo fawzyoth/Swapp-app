6705d16ac8871d700bbbab1a6fa73fe57fb9ad59
Fawzy OTHMAN <fawzy othman>
2025-12-14 19:12:25 +0100

    Update base URL for GitHub Pages and fix QR code URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Swapp-test-main/", // Change this to your repo name
+  base: "/Swapp-app/", // GitHub repo name
   optimizeDeps: {
     exclude: ["lucide-react"],
   },

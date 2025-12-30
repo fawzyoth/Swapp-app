4588ffa8ae8e4d8f1fad67f66c43807183450638
Fawzy OTHMAN <fawzy othman>
2025-12-30 22:12:31 +0100

    Add Supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://xlwznudjklezjkitzqeg.supabase.co";
+const supabaseAnonKey = "sb_publishable_-7ek508_6a7NwZsOqvOFTA_Pt1BubGq";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
